c058bea4ec8e25cbaaeef739ff356b89aa3fc9b7
SantiagoDt <98756849+SantiagoDt@users.noreply.github.com>
2025-09-11 12:28:08 +0200

    feat(auth-svc): introduce LoginResponse & RegisterResponse DTOs

--- auth-svc/src/main/java/com/santiago/auth/dto/LoginResponse.java
+++ auth-svc/src/main/java/com/santiago/auth/dto/LoginResponse.java
@@ -1,4 +1,24 @@
 package com.santiago.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
 public class LoginResponse {
+
+    private String message;
+    private String username;
+    private String email;
+    private Instant lastLoginAt;
+    private String token;
+
+
 }
--- auth-svc/src/main/java/com/santiago/auth/dto/RegisterResponse.java
+++ auth-svc/src/main/java/com/santiago/auth/dto/RegisterResponse.java
@@ -1,4 +1,16 @@
 package com.santiago.auth.dto;
 
+import lombok.*;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RegisterResponse {
+    private String message;      // "Registration successful"
+    private String username;
+    private String email;
+    private Instant createdAt;
 }
